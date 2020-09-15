3d51a96fe3e0b46a046444b84557941be4750556
Angel Retana <retana.martinez.angel.edaurdo@gmail.com>
2020-09-15 14:59:00 -0500

    Agregue carpteta docs

---
+++ docs/precache-manifest.d9c6c8c1e731e01502a5754cb436bee6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "182124f70a4f0eddb6b1abe77127f948",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d6bd954970e6b57b76a0",
+    "url": "/static/js/2.da903099.chunk.js"
+  },
+  {
+    "revision": "94acd2dd16137b64cf94053598b7d526",
+    "url": "/static/js/2.da903099.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1dd8bb5fb568398d437c",
+    "url": "/static/js/main.62b93d05.chunk.js"
+  },
+  {
+    "revision": "169e6b8370504a33c406",
+    "url": "/static/js/runtime-main.7c8b177a.js"
+  }
+]);
