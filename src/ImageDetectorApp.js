765f21e9b04a0db66aef114919a0d42b486c1bca
Angel Retana <retana.martinez.angel.edaurdo@gmail.com>
2020-09-15 12:56:05 -0500

    Sirve analyzeImage.js

--- src/ImageDetectorApp.js
+++ src/ImageDetectorApp.js
@@ -1,53 +1,45 @@
-import React, { useEffect, useLayoutEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import * as ml5 from 'ml5'
+import { analyzeImage } from './helpers/analyzeImage';
 
 const ImageDetectorApp = () => {
 
     const btn = useRef("btn-add")
     const [dataImage, setDataImage] = useState(null);
-    const [prediction, setPrediction] = useState({loading:true});
-    const {label,confidence,loading} = prediction
-    const analyzeImage = () => {
-        const classifier = ml5.imageClassifier('MobileNet', (load, error) => {
-            console.log("modelo cargado")
-        });
-        classifier.classify(document.getElementById("img"), (err, results) => {
-            if(results){
-                const first = results[0]
-                setPrediction(
-                    {loading:false,
-                    ...first}
-                )
-               
-            }
-        })
+    const [prediction, setPrediction] = useState({ loading: true });
+    const { label, confidence, loading } = prediction
 
-    }
+    const handleClick = () => {
 
-    const handleClick = ()=>{
-        
-        btn.current.addEventListener('change',({target})=>{
+        btn.current.addEventListener('change', ({ target }) => {
             const image = target.files[0]
 
             let readear = new FileReader()
 
-            readear.onload= (({target})=>{
+            readear.onload = (({ target }) => {
                 setDataImage(target.result)
-            })  
+            })
             readear.readAsDataURL(image)
 
         })
     }
 
     useEffect(() => {
-        dataImage && analyzeImage()
+        if (dataImage) {
+            analyzeImage().then(res => {
+                setPrediction({
+                    loading: false,
+                    ...res
+                })
+            })
+        }
     }, [dataImage]);
 
     return (
         <div>
-            {console.log(!!dataImage)}
-            <img id="img" src={(!dataImage) ? "./assets/images/img-icon.png" : dataImage} alt="Imagen" style={{maxWidth:450}}/>
-            {(!loading) &&  <h4>{`${label} --> ${confidence*100}%`}</h4>}
+
+            <img id="img" src={(!dataImage) ? "./assets/images/img-icon.png" : dataImage} alt="Imagen" style={{ maxWidth: 450 }} />
+            {(!loading) && <h4>{`${label} --> ${confidence * 100}%`}</h4>}
 
             <input
                 type="file"
---
+++ src/helpers/analyzeImage.js
@@ -0,0 +1,10 @@
+
+import * as ml5 from 'ml5'
+
+export const analyzeImage = async(img) => {
+   
+    const model = await ml5.imageClassifier('MobileNet');
+    const results = await model.classify(document.getElementById("img"))
+
+    return results[0];
+}
